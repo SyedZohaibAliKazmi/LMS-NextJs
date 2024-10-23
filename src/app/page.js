010626bcf7c2081a58023c375b2721596a78c657
syedzohaibalikazmi <syedzohaibali9900@gmail.com>
2024-10-23 17:55:36 +0500

    LMS Next-JS

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="	text-6xl text-orange-400 text-col  font-bold ">Welcome!</h1>
+    </div>
+  );
+}
